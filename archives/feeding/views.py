febaeb2afad3a70c42a451549ba152b73bf75a1f
Mastanar <mastanar@interia.pl>
2017-03-27 21:31:20 +0200

    (27.03.2017)

--- archives/feeding/views.py
+++ archives/feeding/views.py
@@ -1,12 +1,15 @@
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.views import generic
 from feedjack.models import Post
 
 class IndexView(generic.ListView):
+    model = Post
+    paginate_by = 4
     template_name = 'feeding/index.html'
     context_object_name = 'latest_news_list'
 
     def get_queryset(self):
-        return Post.objects.order_by('-date_modified')[:10]
+        return Post.objects.all().order_by('-date_modified')
 
 class DetailView(generic.DeleteView):
     model = Post
